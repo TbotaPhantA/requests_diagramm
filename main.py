5eeb6c67d63db9ca3256a73a9909c576f198d1a0
botak <botak@botak>
2020-11-20 11:27:18 +0300

    сухой проект

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from starlette.middleware.cors import CORSMiddleware
-
 import json
-
 from clickhouse_driver import Client
 
 app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=["*"])
+origins = [
+    '*'
+]
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["GET"],
+                   allow_headers=["*"], )
 client = Client('52.29.95.195')
 
 
 @app.get("/data_for_graph")
-def read_item(url_pattern, start_date, end_date):
+def get_quantity_of_urls(url_pattern: str, start_date: str, end_date: str):
 
     result = client.execute(
         "SELECT toDate(time) as date, COUNT(url_pattern) as quantity "
         "FROM flypost.flypost_log "
-        "WHERE url_pattern = '{0}' BETWEEN {1} AND {2} "
+        "WHERE url_pattern = '{0}' AND date BETWEEN '{1}' AND '{2}' "
         "GROUP BY date "
         "ORDER BY date;".format(url_pattern, start_date, end_date))
 
     list_of_dicts = []
     for item in result:
-        list_of_dicts.append({"date": str(item[0]), "quantity": str(item[1])})
-
-    return {json.dumps(list_of_dicts)}
+        list_of_dicts.append({"date": str(item[0]), "quantity": int(item[1])})
+    return Response(content=json.dumps(list_of_dicts), media_type="application/json")
